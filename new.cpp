7d4750c33a37a92e21e50a4c56faa9e158e1233e
settidurgapoojitha <114555221+settidurgapoojitha@users.noreply.github.com>
2023-02-18 18:46:31 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "Task 4 succeeded\n";
+  cout << "Assignment-2 completed\n";
+  return 0;
+}
